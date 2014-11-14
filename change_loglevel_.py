f046f16b8eafafa47c9ba4ead28976da0f97d1a6
Boris Yakimov <borisyakimov@ymail.com>
2014-11-14 18:10:10 +0200

    Reworked the backup_function. Should add a timestamp to each file when its backedup.

--- change_loglevel_.py
+++ change_loglevel_.py
@@ -1,6 +1,7 @@
 #/usr/bin/env python
 import subprocess
 import os
+from datetime import datetime # used only to add timestamp to backed up files.
 
 file_to_edit = "logback.xml"
 source_text = "TRACE"
@@ -8,10 +9,18 @@ replace_text = "ERROR"
 processed_files = []  # Array used to keep track of all files that have been processed
 
 
-def backup_file(file_to_backup, backup_dir):
-    """ Get a file and copy it with a different name for backup """
-    backup_config = subprocess.call(['cp', file_to_backup, backup_dir])
-    print "Backing up file %s to folder : %s ! " % (file_to_backup, backup_dir)
+def backup_file(file_to_backup, backup_destination):
+    """ Copy file to backup dir """
+    timestamp = datetime.now().strftime('%Y-%m-%d')
+    path_to_backup = backup_destination + file_to_backup + timestamp
+
+    with open(file_to_backup) as text:
+        with open(path_to_backup, 'w') as backedup_file:
+            for line in text:
+                backedup_file.write(line)
+
+    # subprocess.call(['cp', file_to_backup, backup_destination])
+    print "Backing up file %s to folder : %s ! " % (file_to_backup, backup_destination)
 
 def replace_loglevel(file_to_edit, source_text, replace_text):
     """ Open file and replace the source_text with the replace_text strings """
@@ -23,7 +32,7 @@ def replace_loglevel(file_to_edit, source_text, replace_text):
     file_to_write.write(text_from_original.replace(source_text, replace_text))
     print "Replacing string %s with string %s in file %s" % (source_text, replace_text, file_to_edit)
 
-def backup_and_edit_files(dir_path, backup_dir):
+def backup_and_edit_files(dir_path, backup_destination):
     """ Backup the file and replace the source_text with replace_text """
     for item in os.listdir(dir_path): # Iterate over each dir in the dir_path
         path = os.path.join(dir_path, item) # Create full path to file
@@ -31,20 +40,17 @@ def backup_and_edit_files(dir_path, backup_dir):
             if os.path.isfile(path) and item == file_to_edit: # Match filename to be the same as in file_to_edit
                 print "Matched file %s " % (file_to_edit)
                 print "Backing up the current file - %s - before editing" % (item)
-                backup_file(path, backup_dir)
+                backup_file(path, backup_destination)
                 print "Replacing loglevel from %s to %s " % (source_text, replace_text)
                 replace_loglevel(path, source_text, replace_text)
                 processed_files.append(path)
                 print "Processed - %s" % path
             elif os.path.isdir(path): # Only descend into dirs
-                backup_and_edit_files(path, backup_dir)
+                backup_and_edit_files(path, backup_destination)
 
 if __name__ == '__main__':
     dir_path = ""   # Put full path to dir to search in
-    backup_dir = ""  # Dir to backup to
-    backup_and_edit_files(dir_path, backup_dir)
+    backup_destination = ""  # Dir to backup to
+    backup_and_edit_files(dir_path, backup_destination)
 
-
-
-# Bug with executing the script where there is more than 1 file in each directory
-# Current error -  OSError: [Errno 20] Not a directory: 'path/to/file/fgfd.xml'
+# Fix the backup, have to add a timestamp at each backed up file
