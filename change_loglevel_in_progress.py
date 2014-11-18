5f8f4d98d69363ac3a77d9a0ffe3afb7fe9a5ad2
Boris Yakimov <borisyakimov@ymail.com>
2014-11-18 10:18:36 +0200

    Reworked backup function.

--- change_loglevel_in_progress.py
+++ change_loglevel_in_progress.py
@@ -1,27 +1,16 @@
 #/usr/bin/env python
-import subprocess
 import os
-from datetime import datetime # used only to add timestamp to backed up files.
 
-file_to_edit = "logback.xml"
-source_text = "TRACE"
-replace_text = "ERROR"
-processed_files = []  # Array used to keep track of all files that have been processed
-
-
-def backup_file(file_to_backup, backup_destination):
+def backup_file(file_to_backup):
     """ Copy file to backup dir """
-    timestamp = datetime.now().strftime('%Y-%m-%d')
-    path_to_backup = backup_destination + file_to_backup + timestamp
+    path_to_backup = file_to_backup + "_bkp_eds1057"
 
     with open(file_to_backup) as text:
         with open(path_to_backup, 'w') as backedup_file:
             for line in text:
                 backedup_file.write(line)
                 backedup_file.close()
-
-    # subprocess.call(['cp', file_to_backup, backup_destination])
-    print "Backing up file %s to folder : %s ! " % (file_to_backup, backup_destination)
+    print "Backing up file %s to file : %s ! " % (file_to_backup, path_to_backup)
 
 def replace_loglevel(file_to_edit, source_text, replace_text):
     """ Open file and replace the source_text with the replace_text strings """
@@ -33,13 +22,17 @@ def replace_loglevel(file_to_edit, source_text, replace_text):
     file_to_write.write(text_from_original.replace(source_text, replace_text))
     print "Replacing string %s with string %s in file %s" % (source_text, replace_text, file_to_edit)
 
+file_to_edit = "logback.xml"
+source_text = "TRACE"
+replace_text = "ERROR"
+processed_files = []  # Array used to keep track of all files that have been processed
+
 def backup_and_edit_files(dir_path, backup_destination):
     """ Backup the file and replace the source_text with replace_text """
     for item in os.listdir(dir_path): # Iterate over each dir in the dir_path
         path = os.path.join(dir_path, item) # Create full path to file
         if path not in processed_files:
             if os.path.isfile(path) and item == file_to_edit: # Match filename to be the same as in file_to_edit
-                print "Matched file %s " % (file_to_edit)
                 print "Backing up the current file - %s - before editing" % (item)
                 backup_file(path, backup_destination)
                 print "Replacing loglevel from %s to %s " % (source_text, replace_text)
@@ -54,4 +47,5 @@ if __name__ == '__main__':
     backup_destination = ""  # Dir to backup to
     backup_and_edit_files(dir_path, backup_destination)
 
-# Fix the backup, have to add a timestamp at each backed up file
+# Fix the backup, create a copy of the initial file with a different name in the same dir
+# Move initial vars before the last function
