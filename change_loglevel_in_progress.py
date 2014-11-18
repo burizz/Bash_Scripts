50455c34c8a18a6999f94b71a1a3105705f7612c
Boris Yakimov <borisyakimov@ymail.com>
2014-11-18 10:52:46 +0200

    Shortened the backup functon and tested all functionality.

--- change_loglevel_in_progress.py
+++ change_loglevel_in_progress.py
@@ -1,16 +1,13 @@
 #/usr/bin/env python
 import os
+import shutil
 
 def backup_file(file_to_backup):
     """ Copy file to backup dir """
-    path_to_backup = file_to_backup + "_bkp_eds1057"
+    destination_file = file_to_backup + "_bkp_eds1057"
+    shutil.copyfile(file_to_backup, destination_file)
 
-    with open(file_to_backup) as text:
-        with open(path_to_backup, 'w') as backedup_file:
-            for line in text:
-                backedup_file.write(line)
-                backedup_file.close()
-    print "Backing up file %s to file : %s ! " % (file_to_backup, path_to_backup)
+    print "Backing up file %s to file : %s ! " % (file_to_backup, destination_file)
 
 def replace_loglevel(file_to_edit, source_text, replace_text):
     """ Open file and replace the source_text with the replace_text strings """
@@ -20,6 +17,7 @@ def replace_loglevel(file_to_edit, source_text, replace_text):
 
     file_to_write = open(file_to_edit, 'w')
     file_to_write.write(text_from_original.replace(source_text, replace_text))
+    file_to_write.close()
     print "Replacing string %s with string %s in file %s" % (source_text, replace_text, file_to_edit)
 
 file_to_edit = "logback.xml"
@@ -27,25 +25,30 @@ source_text = "TRACE"
 replace_text = "ERROR"
 processed_files = []  # Array used to keep track of all files that have been processed
 
-def backup_and_edit_files(dir_path, backup_destination):
+def backup_and_edit_files(dir_path):
     """ Backup the file and replace the source_text with replace_text """
     for item in os.listdir(dir_path): # Iterate over each dir in the dir_path
         path = os.path.join(dir_path, item) # Create full path to file
         if path not in processed_files:
             if os.path.isfile(path) and item == file_to_edit: # Match filename to be the same as in file_to_edit
                 print "Backing up the current file - %s - before editing" % (item)
-                backup_file(path, backup_destination)
+                backup_file(path)
                 print "Replacing loglevel from %s to %s " % (source_text, replace_text)
                 replace_loglevel(path, source_text, replace_text)
                 processed_files.append(path)
                 print "Processed - %s" % path
             elif os.path.isdir(path): # Only descend into dirs
-                backup_and_edit_files(path, backup_destination)
+                backup_and_edit_files(path)
 
 if __name__ == '__main__':
     dir_path = ""   # Put full path to dir to search in
-    backup_destination = ""  # Dir to backup to
-    backup_and_edit_files(dir_path, backup_destination)
+    backup_and_edit_files(dir_path)
 
-# Fix the backup, create a copy of the initial file with a different name in the same dir
-# Move initial vars before the last function
+"""
+Runtime:
+
+real	0m0.017s
+user	0m0.007s
+sys	    0m0.006s
+
+"""
