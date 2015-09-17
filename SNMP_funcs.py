9f35f7a924f00e3e5a8ff86837fed57465da9d83
burizz <borisyakimov@ymail.com>
2015-09-17 17:30:02 +0300

    Update SNMP_funcs.py

--- SNMP_funcs.py
+++ SNMP_funcs.py
@@ -1,1 +1,8 @@
+import netsnmp
 
+oid = netsnmp.Varbind('sysDescr')
+result = netsnmp.snmpwalk(oid,
+                      Version = 2,
+                      DestHost = "localhost",
+                      Community= "public")
+                      
