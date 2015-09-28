60296d19ad0b7094dc0d4423c66e81ccd7036109
burizz <borisyakimov@ymail.com>
2015-09-28 11:34:01 +0300

    Update older_linux_stuff.py

--- older_linux_stuff.py
+++ older_linux_stuff.py
@@ -65,6 +65,43 @@ def get_gids():
         gids.append(item[gid_index])
 	
     print gids
+   
+   
+def argv_excercise1():
+    """ Exercies from Learn Python the Hard Way """
+    from sys import argv
+    
+    script, filename = argv
+    
+    print "We're going to erase %r." % filename
+    print "If you don't want that, hit CTRL-C (^C)."
+    print "If you do want that, hit RETURN."
+    
+    raw_input("?")
+    
+    print  "Opening the file..."
+    target = open(filename, 'w')
+    
+    print "Truncating the file. Goodbye!"
+    target.truncate()
+    
+    print "Now I'm going to ask you for three lines."
+    
+    line1 = raw_input("line1: ")
+    line2 = raw_input("line2: ")
+    line3 = raw_input("line3: ")
+    
+    print "I'm going to write these to the file."
+    
+    target.write(line1)
+    target.write("\n")
+    target.write(line2)
+    target.write("\n")
+    target.write(line3)
+    target.write("\n")
+    
+    print "And finally, we close it."
+    target.close()
 
 if __name__ == '__main__':
     edit_user_attrib('users.txt')
