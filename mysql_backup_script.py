12df30311170c97c3369b5e15b845c05b30ac812
burizz <borisyakimov@ymail.com>
2016-01-17 20:07:08 +0200

    Update mysql_backup_script.py

--- mysql_backup_script.py
+++ mysql_backup_script.py
@@ -1,15 +1,16 @@
-def dump_database(db_host, db_user, db_pass, db_name, sql_dump_filename):
-    args = ['mysqldump',
-            '-h', db_host,
-            '-u', db_user,
-            '-p' + db_pass,
-            '--max_allowed_packet=512M',
-            '--all-databases',
-            '--ignore-table=mysql.event',
-            '>',
-            sql_dump_filename]
+#!/usr/bin/env python
+
+import subprocess
+
+def dump_database(db_host, db_user, db_pass, sql_dump_filename):
+    args = ["mysqldump", "-h", db_host, "-u", db_user, "-p" + db_pass, "--max_allowed_packet=512M", "--all-databases", "--ignore-table=mysql.event"]
 
     with open(sql_dump_filename, 'w', 0) as f:
         p = subprocess.Popen(args, stdout=subprocess.PIPE)
-        f.write(p.stdout.read())
+        f.write(p.communicate()[0])
         f.close()
+
+        return p.returncode
+
+if __name__ == "__main__":
+    print dump_database('localhost', 'root', '1234', 'dump_db.sql')
