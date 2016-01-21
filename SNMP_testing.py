d41220f65bab0175ebf50adb2fa3d03b516f1a16
burizz <borisyakimov@ymail.com>
2016-01-21 22:22:29 +0200

    Delete SNMP_testing.py

--- SNMP_testing.py
+++
@@ -1,8 +0,0 @@
-import netsnmp
-
-oid = netsnmp.Varbind('sysDescr')
-result = netsnmp.snmpwalk(oid,
-                      Version = 2,
-                      DestHost = "localhost",
-                      Community= "public")
-                      
