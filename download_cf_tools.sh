6535c6031c36825006cc141c81fedbb83a92864e
Boris Yakimov <borisyakimov@ymail.com>
2016-07-26 12:07:44 +0200

    Update download_cf_tools.sh

--- download_cf_tools.sh
+++ download_cf_tools.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# This script checks the latest available versions of all cf_tools and updates the ones that have newer versions
+# It depends on the following
+#
+# * curl
+# * tar
+# * grep
+
+BASE_DIR=`pwd`
+
 # Get latest versions of each component -----------------------------------------------------------------------------------------------------------------------------------#
 
 # cf_uaac
@@ -25,17 +34,17 @@ mysql_plugin_version=$(curl -k -L https://api.github.com/repos/pivotalservices/c
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
-### CF UAAC ###
+### Update CF UAAC if a newer version is available ###
 
 # Check if version is newest
 if $cf_uaac_version
 
 # Download latest cf_uaac_version
-curl -k -L -o cf-uaac-tar.gz $(curl -k -L https://api.github.com/repos/cloudfoundry/cf-uaac/releases/latest | grep "tarball_url" |grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*")
+curl -k -L -o cf-uaac-$cf_uaac_version.tar.gz $(curl -k -L https://api.github.com/repos/cloudfoundry/cf-uaac/releases/latest | grep "tarball_url" |grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*")
 
 ###  ###
 
-### CF CLI ###
+### Update CF CLI and its Plugins if newer versions are available ###
 
 # Check if version is newest
 if $cf_cli_version
@@ -48,39 +57,38 @@ curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github"
 
 ### ###
 
-### CFOPS ###
+### Update CFOPS and its Plugins if newer versions are available ###
 
 # Check if version is newest
 if $cfops_version
 
 # Download latest cfops binary 
-curl -v -k -L -O $(curl -s -k -L https://api.github.com/repos/pivotalservices/cfops/releases/latest | grep cfops_binaries.tgz | grep browser_download_url | grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*")
+curl -v -k -L -o cfops_binaries-$cfops_version.tgz $(curl -s -k -L https://api.github.com/repos/pivotalservices/cfops/releases/latest | grep cfops_binaries.tgz | grep browser_download_url | grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*")
 
 ## NFS-Plugin
 
 # Check if version is newest
 if $cfops_nfs_plugin_version
 
-curl -v -k -L -O $(curl -s -k -L https://api.github.com/repos/pivotalservices/cfops-nfs-plugin/releases/latest | grep cfops-nfs-plugin_binaries.tgz | grep browser_download_url | grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*")
+curl -v -k -L -o cfops-nfs-plugin_binaries-$cfops_nfs_plugin_version.tgz $(curl -s -k -L https://api.github.com/repos/pivotalservices/cfops-nfs-plugin/releases/latest | grep cfops-nfs-plugin_binaries.tgz | grep browser_download_url | grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*")
 
 ## RabbitMQ 
 
 # Check if version is newest
 if $rabbit_mq_plugin_version
 
-curl -v -k -L -O $(curl -s -k -L https://api.github.com/repos/pivotalservices/cfops-rabbitmq-plugin/releases/latest | grep cfops-rabbitmq-plugin_binaries.tgz | grep browser_download_url | grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*")
+curl -v -k -L -o cfops-nfs-plugin_binaries-$rabbit_mq_plugin_version.tgz $(curl -s -k -L https://api.github.com/repos/pivotalservices/cfops-rabbitmq-plugin/releases/latest | grep cfops-rabbitmq-plugin_binaries.tgz | grep browser_download_url | grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*")
 
 ## Redis plugin
 
 # Check if version is newest
 if $redis_plugin_version
 
-curl -v -k -L -O $(curl -s -k -L https://api.github.com/repos/pivotalservices/cfops-redis-plugin/releases/latest | grep cfops-redis-plugin_binaries.tgz | grep browser_download_url | grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*")
-
+curl -v -k -L -o cfops-redis-plugin_binaries-$redis_plugin_version.tgz $(curl -s -k -L https://api.github.com/repos/pivotalservices/cfops-redis-plugin/releases/latest | grep cfops-redis-plugin_binaries.tgz | grep browser_download_url | grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*")
 
 ## MySQL plugin
 
 # Check if version is newest
 if $mysql_plugin_version
 
-curl -v -k -L -O $(curl -s -k -L https://api.github.com/repos/pivotalservices/cfops-mysql-plugin/releases/latest | grep cfops-mysql-plugin_binaries.tgz | grep browser_download_url | grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*")
+curl -v -k -L -o cfops-mysql-plugin_binaries-$mysql_plugin_version.tgz $(curl -s -k -L https://api.github.com/repos/pivotalservices/cfops-mysql-plugin/releases/latest | grep cfops-mysql-plugin_binaries.tgz | grep browser_download_url | grep -Eo "(http|https)://[\da-z./?A-Z0-9\D=_-]*")
